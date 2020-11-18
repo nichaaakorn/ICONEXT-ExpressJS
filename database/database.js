75669812ade9b9b5a6bb6b629a841d886b82fdea
nichakorn <s6030213039@phuket.psu.ac.th>
2020-11-18 15:09:18 +0700

    Add Project ICONEXT ExpressJS

---
+++ database/database.js
@@ -0,0 +1,496 @@
+const sql = require("mssql");
+
+// config for your database
+var config = {
+    user: "sa",
+    password: "123456",
+    server: "DESKTOP-IUI17M2", // You can use 'localhost\\instance' to connect to named instance
+    database: "ICONEXTContext-1",
+    options: {
+        "enableArithAbort": true,
+        "encrypt": true,
+
+    },
+}
+
+async function getProject() {
+    try {
+        const pool = await sql.connect(config);
+        const query =
+            `select * from Project`;
+        const result = await pool.request()
+            .query(query)
+        return result
+    } catch (err) {
+        console.log("MESSAGE " + err.message);
+    }
+}
+
+async function getProjectByID(ID) {
+    try {
+        const pool = await sql.connect(config);
+        const query = `select * from project where PID = ${ID}`;
+        const result = await pool.request()
+            .query(query)
+        return result
+    } catch (err) {
+        console.log("MESSAGE " + err.message);
+    }
+}
+
+
+async function addProject(newProject) {
+    try {
+        const pool = await sql.connect(config);
+        const query =
+            `INSERT INTO Project (Name,Partner,Customer,StartDate)
+            VALUES('${newProject[0]}', '${newProject[1]}', '${newProject[2]}', '${newProject[3]}');`
+        const result = await pool.request()
+            .query(query)
+        return result
+    } catch (err) {
+        console.log("MESSAGE " + err.message);
+    }
+}
+
+async function updateProject(project) {
+    try {
+        const pool = await sql.connect(config);
+        const query =
+            `UPDATE Project
+            SET Name = '${project[1]}', Partner = '${project[2]}', Customer = '${project[3]}', StartDate = '${project[4]}'
+            WHERE PID = ${project[0]};`
+        const result = await pool.request()
+            .query(query)
+        return result
+    } catch (err) {
+        console.log("MESSAGE " + err.message);
+    }
+}
+
+async function deleteProject(ID) {
+    try {
+        const pool = await sql.connect(config);
+        const query = `delete from project where PID = ${ID}`;
+        const result = await pool.request()
+            .query(query)
+        return result
+    } catch (err) {
+        console.log("MESSAGE " + err.message);
+    }
+}
+
+async function getTasksProject(ID) {
+    try {
+        const pool = await sql.connect(config);
+        const query = `select * from TasksProject where PID = ${ID}`;
+        const result = await pool.request()
+            .query(query)
+        return result
+    } catch (err) {
+        console.log("MESSAGE " + err.message);
+    }
+}
+
+async function getOneTasksProject(ID) {
+    try {
+        const pool = await sql.connect(config);
+        const query = `select Top 1 * from TasksProject where PID = ${ID}`;
+        const result = await pool.request()
+            .query(query)
+        return result.recordset
+    } catch (err) {
+        console.log("MESSAGE " + err.message);
+    }
+}
+
+async function getTasksByTID(ID) {
+    try {
+        const pool = await sql.connect(config);
+        const query = `select * from TasksProject where TID = ${ID}`;
+        const result = await pool.request()
+            .query(query)
+        return result
+    } catch (err) {
+        console.log("MESSAGE " + err.message);
+    }
+}
+
+async function getTasksByName(Name) {
+    try {
+        const pool = await sql.connect(config);
+        const query = `select * from TasksProject where Tasks = '${Name}'`;
+        const result = await pool.request()
+            .query(query)
+        return result
+    } catch (err) {
+        console.log("MESSAGE " + err.message);
+    }
+}
+
+async function addTasks(newTask) {
+    try {
+        const pool = await sql.connect(config);
+        const query =
+            `INSERT INTO TasksProject (Tasks,PID)
+            VALUES('${newTask[0]}', '${newTask[1]}');`
+        const result = await pool.request()
+            .query(query)
+        return result
+    } catch (err) {
+        console.log("MESSAGE " + err.message);
+    }
+}
+
+async function deleteTask(ID) {
+    try {
+        const pool = await sql.connect(config);
+        const query = `delete from TasksProject where TID = ${ID}`;
+        const result = await pool.request()
+            .query(query)
+        return result
+    } catch (err) {
+        console.log("MESSAGE " + err.message);
+    }
+}
+
+async function getPosition() {
+    try {
+        const pool = await sql.connect(config);
+        const query = `select * from Positions`;
+        const result = await pool.request()
+            .query(query)
+        return result
+    } catch (err) {
+        console.log("MESSAGE " + err.message);
+    }
+}
+
+async function addPosition(newPosition) {
+    try {
+        const pool = await sql.connect(config);
+        const query =
+            `INSERT INTO Positions (Position,Description)
+        VALUES ('${newPosition[0]}','${newPosition[1]}')`;
+        const result = await pool.request()
+            .query(query)
+        return result
+    } catch (err) {
+        console.log("MESSAGE " + err.message);
+    }
+}
+
+async function deletePosition(ID) {
+    try {
+        const pool = await sql.connect(config);
+        const query = `delete from Positions where PosID = ${ID}`;
+        const result = await pool.request()
+            .query(query)
+        return result
+    } catch (err) {
+        console.log("MESSAGE " + err.message);
+    }
+}
+
+async function getStaff() {
+    try {
+        const pool = await sql.connect(config);
+        const query = `select * from Employee`;
+        const result = await pool.request()
+            .query(query)
+        return result
+    } catch (err) {
+        console.log("MESSAGE " + err.message);
+    }
+}
+
+async function getStaffByID(ID) {
+    try {
+        const pool = await sql.connect(config);
+        const query = `select * from Employee where EID = '${ID}'`;
+        const result = await pool.request()
+            .query(query)
+        return result
+    } catch (err) {
+        console.log("MESSAGE " + err.message);
+    }
+}
+
+async function addStaff(newstaff) {
+    try {
+        const pool = await sql.connect(config);
+        const query = `INSERT INTO Employee
+        (EID, Title, Name, SurName, NickName, Position, Tel, Email, StartDate, EndDate, Active)
+        VALUES('${newstaff[0]}', '${newstaff[1]}', '${newstaff[2]}', '${newstaff[3]}', '${newstaff[4]}', 
+        '${newstaff[5]}', '${newstaff[6]}', '${newstaff[7]}', '${newstaff[8]}', '${newstaff[9]}', '${newstaff[10]}');`;
+        const result = await pool.request()
+            .query(query)
+        return result
+    } catch (err) {
+        console.log("MESSAGE " + err.message);
+    }
+}
+
+async function updateStaff(staff) {
+    try {
+        const pool = await sql.connect(config);
+        const query =
+            `UPDATE Employee SET 
+            EID = '${staff[1]}', 
+            Title = '${staff[2]}', 
+            Name = '${staff[3]}', 
+            SurName = '${staff[4]}', 
+            NickName = '${staff[5]}',
+            Position = '${staff[6]}',
+            Tel = '${staff[7]}',
+            Email = '${staff[8]}',
+            StartDate = '${staff[9]}',
+            EndDate = '${staff[10]}',
+            Active = '${staff[11]}'
+            WHERE EID = '${staff[0]}'`
+        const result = await pool.request()
+            .query(query)
+        return result
+    } catch (err) {
+        console.log("MESSAGE " + err.message);
+    }
+}
+
+async function deleteStaff(ID) {
+    try {
+        const pool = await sql.connect(config);
+        const query = `delete from Employee where EID = ${ID}`;
+        const result = await pool.request()
+            .query(query)
+        return result
+    } catch (err) {
+        console.log("MESSAGE " + err.message);
+    }
+}
+
+async function getOutsouce() {
+    try {
+        const pool = await sql.connect(config);
+        const query = `select * from outsource`;
+        const result = await pool.request()
+            .query(query)
+        return result
+    } catch (err) {
+        console.log("MESSAGE " + err.message);
+    }
+}
+
+async function getOutsouceByID(ID) {
+    try {
+        const pool = await sql.connect(config);
+        const query = `select * from outsource where ID = '${ID}'`;
+        const result = await pool.request()
+            .query(query)
+        return result
+    } catch (err) {
+        console.log("MESSAGE " + err.message);
+    }
+}
+
+async function addOutsource(newstaff) {
+    try {
+        const pool = await sql.connect(config);
+        const query = `INSERT INTO Outsource
+        (ID, Title, Name, SurName, NickName, Position, Tel, Email, StartDate, EndDate, Active)
+        VALUES('${newstaff[0]}', '${newstaff[1]}', '${newstaff[2]}', '${newstaff[3]}', '${newstaff[4]}', 
+        '${newstaff[5]}', '${newstaff[6]}', '${newstaff[7]}', '${newstaff[8]}', '${newstaff[9]}', '${newstaff[10]}');`;
+        const result = await pool.request()
+            .query(query)
+        return result
+    } catch (err) {
+        console.log("MESSAGE " + err.message);
+    }
+}
+
+async function updateOutsource(staff) {
+    try {
+        const pool = await sql.connect(config);
+        const query =
+            `UPDATE Outsource SET 
+            ID = '${staff[1]}', 
+            Title = '${staff[2]}', 
+            Name = '${staff[3]}', 
+            SurName = '${staff[4]}', 
+            NickName = '${staff[5]}',
+            Position = '${staff[6]}',
+            Tel = '${staff[7]}',
+            Email = '${staff[8]}',
+            StartDate = '${staff[9]}',
+            EndDate = '${staff[10]}',
+            Active = '${staff[11]}'
+            WHERE ID = '${staff[0]}'`
+        const result = await pool.request()
+            .query(query)
+        return result
+    } catch (err) {
+        console.log("MESSAGE " + err.message);
+    }
+}
+
+async function deleteOutsource(ID) {
+    try {
+        const pool = await sql.connect(config);
+        const query = `delete from Outsource where ID = '${ID}'`;
+        const result = await pool.request()
+            .query(query)
+        return result
+    } catch (err) {
+        console.log("MESSAGE " + err.message);
+    }
+}
+
+async function getPhaseTasks(ID) {
+    try {
+        const pool = await sql.connect(config);
+        const query = `select * from Phase where TID = ${ID}`;
+        const result = await pool.request()
+            .query(query)
+        return result
+    } catch (err) {
+        console.log("MESSAGE " + err.message);
+    }
+}
+
+async function addPhase(newPhase) {
+    try {
+        const pool = await sql.connect(config);
+        const query =
+            `INSERT INTO Phase (Phase,StartDate,EndDate,Manpower,Role,Usage,TID)
+        VALUES ('${newPhase[0]}','${newPhase[1]}','${newPhase[2]}','${newPhase[3]}','${newPhase[4]}','${newPhase[5]}','${newPhase[6]}')`;
+        const result = await pool.request()
+            .query(query)
+        return result
+    } catch (err) {
+        console.log("MESSAGE " + err.message);
+    }
+}
+
+async function deletePhase(ID) {
+    try {
+        const pool = await sql.connect(config);
+        const query = `delete from Phase where ID = ${ID}`;
+        const result = await pool.request()
+            .query(query)
+        return result
+    } catch (err) {
+        console.log("MESSAGE " + err.message);
+    }
+}
+
+async function deletePhaseByTID(ID) {
+    try {
+        const pool = await sql.connect(config);
+        const query = `delete from Phase where TID = ${ID}`;
+        const result = await pool.request()
+            .query(query)
+        return result
+    } catch (err) {
+        console.log("MESSAGE " + err.message);
+    }
+}
+
+async function getLeaveByID(ID) {
+    try {
+        const pool = await sql.connect(config);
+        const query = `select * from Leave where ID = '${ID}'`;
+        const result = await pool.request()
+            .query(query)
+        return result
+    } catch (err) {
+        console.log("MESSAGE " + err.message);
+    }
+}
+
+async function addLeave(newLeave) {
+    try {
+        const pool = await sql.connect(config);
+        const query =
+            `INSERT INTO Leave (StartDate,EndDate,Annotation,ID)
+        VALUES ('${newLeave[0]}','${newLeave[1]}','${newLeave[2]}','${newLeave[3]}')`;
+        const result = await pool.request()
+            .query(query)
+        return result
+    } catch (err) {
+        console.log("MESSAGE " + err.message);
+    }
+}
+
+async function getLeaveNewInsert() {
+    try {
+        const pool = await sql.connect(config);
+        const query = `SELECT TOP 1 LID , DATEDIFF(day, StartDate, EndDate) AS Day FROM Leave ORDER BY LID DESC`;
+        const result = await pool.request()
+            .query(query)
+        return result
+    } catch (err) {
+        console.log("MESSAGE " + err.message);
+    }
+}
+
+async function updateLeaveDay(newDay) {
+    try {
+        const pool = await sql.connect(config);
+        const query =
+            `Update Leave SET
+            Days = '${newDay.Day}'
+            Where LID = ${newDay.LID}`;
+        const result = await pool.request()
+            .query(query)
+        return result
+    } catch (err) {
+        console.log("MESSAGE " + err.message);
+    }
+}
+
+async function deleteLeaveByLID(ID) {
+    try {
+        const pool = await sql.connect(config);
+        const query = `delete from Leave where LID = ${ID}`;
+        const result = await pool.request()
+            .query(query)
+        return result
+    } catch (err) {
+        console.log("MESSAGE " + err.message);
+    }
+}
+
+module.exports = {
+    getProject,
+    addStaff,
+    getProjectByID,
+    getOneTasksProject,
+    addProject,
+    addTasks,
+    updateProject,
+    deleteProject,
+    getTasksProject,
+    getTasksByName,
+    deleteTask,
+    getPosition,
+    addPosition,
+    getStaff,
+    getStaffByID,
+    deleteStaff,
+    updateStaff,
+    deletePosition,
+    getOutsouce,
+    addOutsource,
+    deleteOutsource,
+    getOutsouceByID,
+    updateOutsource,
+    getPhaseTasks,
+    addPhase,
+    getTasksByTID,
+    deletePhase,
+    deletePhaseByTID,
+    addLeave,
+    getLeaveByID,
+    getLeaveNewInsert,
+    updateLeaveDay,
+    deleteLeaveByLID,
+}
---
+++ routes/index.js
@@ -0,0 +1,317 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser')
+const db = require("../database/database");
+
+router.use(bodyParser.urlencoded({ extended: true }));
+
+/* GET home page. */
+router.get('/project', async function (req, res, next) {
+  const result = await db.getProject();
+  res.render('index', { Projects: result.recordset });
+});
+
+router.post('/getOneTID', async function (req, res, next) {
+  const PID = req.body.PID;
+  const getOneTID = await db.getOneTasksProject(PID);
+  let TID;
+  if (getOneTID.length == 0) {
+    TID = 0;
+  } else {
+    TID = getOneTID[0].TID
+  }
+  res.redirect(`/project/viewproject/${PID}/${TID}`);
+});
+
+router.post('/createproject', async function (req, res, next) {
+  const projectName = req.body.projectName;
+  const partnerProject = req.body.partnerProject;
+  const custtomerProject = req.body.customerProject;
+  const dateProject = req.body.dateProject;
+  const addProject = [projectName, partnerProject, custtomerProject, dateProject];
+  await db.addProject(addProject);
+  res.redirect('/project');
+});
+
+router.get('/project/editproject/:PID', async function (req, res, next) {
+  const PID = req.params.PID;
+  const result = await db.getProjectByID(PID);
+  res.render('EditProject', { Projects: result.recordset[0] });
+});
+
+router.get('/project/viewproject/:PID/:TID', async function (req, res, next) {
+  const PID = req.params.PID;
+  const TID = req.params.TID;
+  const getprojectByID = await db.getProjectByID(PID);
+  const getTasksProject = await db.getTasksProject(PID);
+  const getTasksByTID = await db.getTasksByTID(TID);
+  const getPhase = await db.getPhaseTasks(TID);
+  const getStaff = await db.getStaff();
+  const getOutsouce = await db.getOutsouce();
+  const getPosition = await db.getPosition();
+  var output;
+  if (getTasksByTID.recordset.length == 0) {
+    output = {
+      TID: 0,
+      Tasks: "",
+      PID: PID,
+    };
+  } else {
+    output = {
+      TID: getTasksByTID.recordset[0].TID,
+      Tasks: getTasksByTID.recordset[0].Tasks,
+      PID: getTasksByTID.recordset[0].PID,
+    };
+  }
+  res.render('ViewProject',
+    {
+      Projects: getprojectByID.recordset[0], Tasks: getTasksProject.recordset, Phases: getPhase.recordset,
+      Outsource: getOutsouce.recordset, Staffs: getStaff.recordset, oneTask: output,
+      Positions: getPosition.recordset
+    }
+  );
+});
+
+router.post('/createtasks', async function (req, res, next) {
+  const PID = req.body.PID;
+  const TaskName = req.body.tasksName;
+  const addTask = [TaskName, PID];
+  await db.addTasks(addTask);
+  const urlTID = await db.getTasksByName(TaskName);
+  res.redirect(`/project/viewproject/${PID}/${urlTID.recordset[0].TID}`);
+});
+
+router.post('/deletetasks', async function (req, res, next) {
+  const PID = req.body.PID;
+  const TID = req.body.deleteTID;
+  var urlTID = await db.getOneTasksProject(PID);
+  if (urlTID == TID) {
+    urlTID == 0;
+  }
+  await db.deleteTask(TID);
+  await db.deletePhaseByTID(TID);
+  res.redirect(`/project/viewproject/${PID}/${urlTID[0].TID}`);
+});
+
+router.post('/createphase', async function (req, res, next) {
+  const PID = req.body.PID;
+  const Phase = req.body.Phase;
+  const StartDate = req.body.StartDate;
+  const EndDate = req.body.EndDate;
+  const Manpower = req.body.Manpower;
+  const Role = req.body.Position;
+  const Usage = req.body.Usage;
+  const TID = req.body.TID;
+  const addPhase = [Phase, StartDate, EndDate, Manpower, Role, Usage, TID]
+  await db.addPhase(addPhase);
+  res.redirect(`/project/viewproject/${PID}/${TID}`);
+});
+
+router.post('/deletephase', async function (req, res, next) {
+  const PID = req.body.PID;
+  const TID = req.body.TID;
+  const phaseID = req.body.phaseID;
+  await db.deletePhase(phaseID);
+  res.redirect(`/project/viewproject/${PID}/${TID}`);
+});
+
+router.post('/editproject', async function (req, res, next) {
+  const PID = req.body.PID;
+  const projectName = req.body.editProjectName;
+  const partnerProject = req.body.editParterProject;
+  const custtomerProject = req.body.editCustomerProject;
+  const dateProject = req.body.editDateProject;
+  const updateProject = [PID, projectName, partnerProject, custtomerProject, dateProject];
+  await db.updateProject(updateProject);
+  res.redirect('/project');
+});
+
+router.post('/deleteproject', async function (req, res, next) {
+  const PID = req.body.PID;
+  await db.deleteProject(PID);
+  await db.deleteTask(PID);
+  res.redirect('/project');
+});
+
+router.get('/manpower/staff', async function (req, res, next) {
+  const staff = await db.getStaff();
+  const postion = await db.getPosition();
+  res.render('Staff', { Staffs: staff.recordset, Positions: postion.recordset });
+});
+
+router.get('/manpower/outsource', async function (req, res, next) {
+  const staff = await db.getOutsouce();
+  const postion = await db.getPosition();
+  res.render('Outsource', { Staffs: staff.recordset, Positions: postion.recordset });
+});
+
+router.post('/addstaff', async function (req, res, next) {
+  const ID = req.body.ID;
+  const Title = req.body.Title;
+  const Name = req.body.Name;
+  const Surname = req.body.Surname;
+  const Nickname = req.body.Nickname;
+  const Position = req.body.Position;
+  const Tel = req.body.Tel;
+  const Email = req.body.Email;
+  const StartDate = req.body.StartDate;
+  const EndDate = req.body.EndDate;
+  const Active = req.body.Active;
+  const addStaff = [ID, Title, Name, Surname, Nickname, Position, Tel, Email, StartDate, EndDate, Active]
+  await db.addStaff(addStaff);
+  res.redirect('/manpower/staff')
+});
+
+router.post('/addoutsource', async function (req, res, next) {
+  const ID = req.body.ID;
+  const Title = req.body.Title;
+  const Name = req.body.Name;
+  const Surname = req.body.Surname;
+  const Nickname = req.body.Nickname;
+  const Position = req.body.Position;
+  const Tel = req.body.Tel;
+  const Email = req.body.Email;
+  const StartDate = req.body.StartDate;
+  const EndDate = req.body.EndDate;
+  const Active = req.body.Active;
+  const addStaff = [ID, Title, Name, Surname, Nickname, Position, Tel, Email, StartDate, EndDate, Active]
+  await db.addOutsource(addStaff);
+  res.redirect('/manpower/outsource')
+});
+
+router.get('/editstaff/:ID', async function (req, res, next) {
+  const ID = req.params.ID
+  const result = await db.getStaffByID(ID);
+  const postion = await db.getPosition();
+  res.render('EditStaff', { Staffs: result.recordset[0], Positions: postion.recordset });
+});
+
+router.get('/editoutsource/:ID', async function (req, res, next) {
+  const ID = req.params.ID
+  const result = await db.getOutsouceByID(ID);
+  const postion = await db.getPosition();
+  res.render('EditOutsource', { Staffs: result.recordset[0], Positions: postion.recordset });
+});
+
+router.post('/updatestaff', async function (req, res, next) {
+  const editID = req.body.editID;
+  const ID = req.body.ID;
+  const Title = req.body.Title;
+  const Name = req.body.Name;
+  const Surname = req.body.Surname;
+  const Nickname = req.body.Nickname;
+  const Position = req.body.Position;
+  const Tel = req.body.Tel;
+  const Email = req.body.Email;
+  const StartDate = req.body.StartDate;
+  const EndDate = req.body.EndDate;
+  const Active = req.body.Active;
+  const updateStaff = [editID, ID, Title, Name, Surname, Nickname, Position, Tel, Email, StartDate, EndDate, Active]
+  await db.updateStaff(updateStaff);
+  res.redirect('/manpower/staff');
+});
+
+router.post('/updateoutsource', async function (req, res, next) {
+  const editID = req.body.editID;
+  const ID = req.body.ID;
+  const Title = req.body.Title;
+  const Name = req.body.Name;
+  const Surname = req.body.Surname;
+  const Nickname = req.body.Nickname;
+  const Position = req.body.Position;
+  const Tel = req.body.Tel;
+  const Email = req.body.Email;
+  const StartDate = req.body.StartDate;
+  const EndDate = req.body.EndDate;
+  const Active = req.body.Active;
+  const updateStaff = [editID, ID, Title, Name, Surname, Nickname, Position, Tel, Email, StartDate, EndDate, Active]
+  await db.updateOutsource(updateStaff);
+  res.redirect('/manpower/outsource');
+});
+
+router.post('/deletestaff', async function (req, res, next) {
+  const EID = req.body.EID;
+  await db.deleteStaff(EID);
+  res.redirect('/manpower/staff');
+});
+
+router.post('/deleteoutsource', async function (req, res, next) {
+  const ID = req.body.ID;
+  await db.deleteOutsource(ID);
+  res.redirect('/manpower/outsource');
+});
+
+router.get('/position', async function (req, res, next) {
+  const result = await db.getPosition();
+  res.render('Position', { Positions: result.recordset });
+});
+
+router.post('/addposition', async function (req, res, next) {
+  const Position = req.body.Position;
+  const Description = req.body.Description;
+  const addPosition = [Position, Description];
+  await db.addPosition(addPosition);
+  res.redirect('/position');
+});
+
+router.post('/deleteposition', async function (req, res, next) {
+  const PosID = req.body.PosID;
+  await db.deletePosition(PosID);
+  res.redirect('/position');
+});
+
+router.get('/leave/staff', async function (req, res, next) {
+  const staff = await db.getStaff();
+  res.render('LeaveStaff', { Staffs: staff.recordset });
+});
+
+router.get('/leave/outsource', async function (req, res, next) {
+  const staff = await db.getOutsouce();
+  res.render('LeaveOutsource', { Staffs: staff.recordset });
+});
+
+router.get('/viewleave/:ID', async function (req, res, next) {
+  const ID = req.params.ID
+  const getLeaveByID = await db.getLeaveByID(ID);
+  var staff;
+  var output;
+  if (ID.substring(0, 1) == "O") {
+    staff = await db.getOutsouceByID(ID);
+    output = {
+      ID: staff.recordset[0].ID,
+      Name: staff.recordset[0].Name,
+      SurName: staff.recordset[0].SurName,
+    }
+  } else {
+    staff = await db.getStaffByID(ID);
+    output = {
+      ID: staff.recordset[0].EID,
+      Name: staff.recordset[0].Name,
+      SurName: staff.recordset[0].SurName,
+    }
+  }
+  res.render('Leave', { Staffs: output, Leaves: getLeaveByID.recordset })
+});
+
+router.post('/addLeave', async function (req, res, next) {
+  const ID = req.body.ID;
+  const StartDate = req.body.StartDate;
+  const EndDate = req.body.EndDate;
+  const Annotation = req.body.Annotation;
+  const addLeave = [StartDate, EndDate, Annotation, ID]
+  await db.addLeave(addLeave);
+  const getLeave = await db.getLeaveNewInsert();
+  await db.updateLeaveDay(getLeave.recordset[0]);
+  res.redirect(`/viewleave/${ID}`);
+});
+
+router.post('/deleteleave', async function (req, res, next) {
+  const LID = req.body.LID;
+  const ID = req.body.ID;
+  await db.deleteLeaveByLID(LID);
+  res.redirect(`/viewleave/${ID}`);
+});
+
+
+
+module.exports = router;
